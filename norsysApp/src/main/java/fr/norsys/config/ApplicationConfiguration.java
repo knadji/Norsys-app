8b84124735bcfe7dcb067f19273c661980bb7499
karim nadji <knadji@norsys.fr>
2016-10-14 15:28:09 +0200

    Ajout de la connection PostgreSQL

--- norsysApp/src/main/java/fr/norsys/config/ApplicationConfiguration.java
+++ norsysApp/src/main/java/fr/norsys/config/ApplicationConfiguration.java
@@ -1,10 +1,9 @@
 package fr.norsys.config;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,10 +12,27 @@ public class ApplicationConfiguration {
 
     private static final String JNDI_NAME = "java:comp/env/jdbc/bonecp";
 
+    // @Bean
+    // public DataSource dataSource() throws NamingException {
+    // final Context ctx = new InitialContext();
+    // // final JndiDataSourceLookup lookup = new JndiDataSourceLookup();
+    // return (DataSource) ctx.lookup( JNDI_NAME );
+    // }
+
     @Bean
-    public DataSource dataSource() throws NamingException {
-        final Context ctx = new InitialContext();
-        // final JndiDataSourceLookup lookup = new JndiDataSourceLookup();
-        return (DataSource) ctx.lookup( JNDI_NAME );
+    public BasicDataSource dataSource() throws URISyntaxException {
+        final URI dbUri = new URI( System.getenv( "DATABASE_URL" ) );
+
+        final String username = dbUri.getUserInfo().split( ":" )[0];
+        final String password = dbUri.getUserInfo().split( ":" )[1];
+        final String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        final BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl( dbUrl );
+        basicDataSource.setUsername( username );
+        basicDataSource.setPassword( password );
+
+        return basicDataSource;
     }
+
 }
